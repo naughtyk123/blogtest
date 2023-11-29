496fef3b58b2c48f19c7566d9ddd33ab003cd879
naughtyk123 <53645703+naughtyk123@users.noreply.github.com>
2023-11-29 21:25:33 +0530

    Update home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -6,7 +6,7 @@
     <div class="col-lg-8">
         @auth
         <div class="col-lg-12 mb-4">
-            <a style="float:right" type="button" href="{{url('add-post')}}" class="btn btn-success" value="Add Post">Add Post</a>
+            <a  type="button" href="{{url('add-post')}}" class="btn btn-success" value="Add Post">Add Post</a>
         </div>
         @endauth
         <div class="search_results">
