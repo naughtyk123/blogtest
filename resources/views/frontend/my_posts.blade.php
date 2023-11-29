6993560603fdbf93b980ddeadb41dd50c385d877
naughtyk123 <53645703+naughtyk123@users.noreply.github.com>
2023-11-29 21:25:45 +0530

    Update my_posts.blade.php

--- resources/views/frontend/my_posts.blade.php
+++ resources/views/frontend/my_posts.blade.php
@@ -6,7 +6,7 @@
     <div class="col-lg-12">
         @auth
         <div class="col-lg-12 mb-4">
-            <a style="float:right" type="button" href="{{url('add-post')}}" class="btn btn-success" value="Add Post">Add Post</a>
+            <a  type="button" href="{{url('add-post')}}" class="btn btn-success" value="Add Post">Add Post</a>
         </div>
         @endauth
         @include('frontend.post_card')
